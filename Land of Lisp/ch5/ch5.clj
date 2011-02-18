016ab1e491f4862db9bf197a7d9841d0e54e3f65
Benny Tsai <benny.tsai@gmail.com>
2011-02-17 23:11:04 -0700

    Land of Lisp chapter 5

---
+++ Land of Lisp/ch5/ch5.clj
@@ -0,0 +1,49 @@
+(use 'wizard)
+(require '[clojure.string :as str])
+
+(nodes "garden")
+
+(describe-location "living-room" nodes)
+
+(describe-path ["garden" "west" "door"])
+
+(describe-paths "living-room" edges)
+
+(edges "living-room")
+
+(map describe-path [["garden" "west" "door"] ["attic" "upstairs" "ladder"]])
+
+(map #(Math/sqrt %) [1 2 3 4 5])
+
+(map first [[:foo :bar] [:baz :qux]])
+
+;; does not work in Clojure, which is a Lisp-1
+(let [car "Honda Civic"]
+  (map car [[:foo :bar] [:baz :qux]]))
+
+(str/join " " ["mary had" "a" "little lamb"])
+
+(str/join " " ["THERE IS A DOOR GOING WEST FROM HERE."
+	       "THERE IS A LADDER GOING UPSTAIRS FROM HERE."])
+
+(objects-at "living-room" objects @object-locations)
+
+(describe-objects "living-room" objects @object-locations)
+
+(look)
+
+(some #(if (= (second %) :y) %) [[5 :x] [3 :y] [7 :z]])
+
+(walk "west")
+
+(def foo (atom '(1 2 3)))
+
+(swap! foo conj 7)
+
+@foo
+
+(walk "east")
+
+(pickup "whiskey")
+
+(inventory)
---
+++ Land of Lisp/ch5/wizard.clj
@@ -0,0 +1,60 @@
+(ns wizard
+  (:require [clojure.string :as str]))
+
+(def nodes {"living-room" "you are in the living-room. a wizard is snoring loudly on the couch."
+	    "garden" "you are in a beautiful garden. there is a well in front of you."
+	    "attic" "you are in the attic. there is a giant welding torch in the corner."})
+
+(def edges {"living-room" [["garden" "west" "door"]
+			   ["attic" "upstairs" "ladder"]]
+	    "garden" [["living-room" "east" "door"]]
+	    "attic" [["living-room" "downstairs" "ladder"]]})
+
+(def objects ["whiskey" "bucket" "frog" "chain"])
+
+(def object-locations (atom {"whiskey" "living-room"
+			     "bucket" "living-room"
+			     "chain" "garden"
+			     "frog" "garden"}))
+
+(def location (atom "living-room"))
+
+(defn describe-location [location nodes]
+  (nodes location))
+
+(defn describe-path [[dest direction thing]]
+  (str "there is a " thing " going " direction " from here."))
+
+(defn describe-paths [location edges]
+  (str/join " " (map describe-path (edges location))))
+
+(defn objects-at [loc objs obj-locs]
+  (let [at-loc? #(= (obj-locs %) loc)]
+    (filter at-loc? objs)))
+
+(defn describe-objects [loc objs obj-locs]
+  (let [describe-obj #(str "you see a " % " on the floor.")]
+    (str/join " " (map describe-obj (objects-at loc objs obj-locs)))))
+
+(defn look []
+  (str/join " " [(describe-location @location nodes)
+		 (describe-paths @location edges)
+		 (describe-objects @location objects @object-locations)]))
+
+(defn walk [direction]
+  (let [next (some #(if (= (second %) direction) %) (edges @location))]
+    (if next
+      (do
+	(reset! location (first next))
+	(look))
+      "you cannot go that way.")))
+
+(defn pickup [object]
+  (if (some #{object} (objects-at @location objects @object-locations))
+    (do
+      (swap! object-locations assoc object "body")
+      (str "you are now carrying the " object "."))
+    "you cannot get that."))
+
+(defn inventory []
+  (str/join " " (cons "ITEMS:" (objects-at "body" objects @object-locations))))
