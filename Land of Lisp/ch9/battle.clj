2884087fe0727c95f600b7bba8687475d45ed8c8
Benny Tsai <benny.tsai@gmail.com>
2011-03-06 18:36:49 -0700

    Land of Lisp chapter 9

---
+++ Land of Lisp/ch9/battle.clj
@@ -0,0 +1,195 @@
+(ns battle
+  (:use [clojure.contrib.seq :only (indexed)]))
+
+(def *health* (atom nil))
+(def *agility* (atom nil))
+(def *strength* (atom nil))
+(def *foes* (atom []))
+(def *foe-builders* (atom []))
+(def *foes-num* 12)
+
+(defn randval [n]
+  (inc (rand-int (max 1 n))))
+
+(defn foe-dead? [f]
+  (<= (:health f) 0))
+
+(defn foes-dead? []
+  (every? foe-dead? @*foes*))
+
+;; Base foe multimethods
+(defmulti foe-hit (fn [[idx f] x] (class f)))
+(defmethod foe-hit ::foe [[idx f] x]
+  (let [new-f (update-in f [:health] - x)
+	name (subs (.getName (class f)) 7)]
+    (if (foe-dead? new-f)
+      (println "You killed the" name)
+      (println "You hit the" name "for" x "health!"))
+    (swap! *foes* assoc idx new-f)))
+
+(defmulti foe-show class)
+(defmethod foe-show ::foe [f]
+  (str "A fierce " (subs (.getName (class f)) 7)))
+
+(defmulti foe-attack (fn [idx f] (class f)))
+
+;; Orc
+(defrecord orc [health club-level])
+(defn make-orc []
+  (orc. (randval 10) (randval 8)))
+(derive orc ::foe)
+(swap! *foe-builders* conj make-orc)
+
+(defmethod foe-show orc [f]
+  (str "A wicked orc with a level " (:club-level f) " club."))
+
+(defmethod foe-attack orc [_ f]
+  (let [x (randval (:club-level f))]
+    (println "An orc swings his club and knocks off" x "of your health.")
+    (swap! *health* - x)))
+
+;; Hydra
+(defrecord hydra [health])
+(defn make-hydra []
+  (hydra. (randval 10)))
+(derive hydra ::foe)
+(swap! *foe-builders* conj make-hydra)
+
+(defmethod foe-show hydra [f]
+  (str "A malicious hydra with " (:health f) " heads."))
+
+(defmethod foe-hit hydra [[idx f] x]
+  (let [new-f (update-in f [:health] - x)]
+    (if (foe-dead? new-f)
+      (println "The corpse of the decapitated hydra falls to the floor!")
+      (println "You lop off" x "of the hydra's head!"))
+    (swap! *foes* assoc idx new-f)))
+
+(defmethod foe-attack hydra [idx f]
+  (let [x (randval (quot (:health f) 2))]
+    (println "A hydra attacks you with" x "of its heads for" x "health!")
+    (println "It also grows back 1 more head!")
+    (swap! *foes* update-in [idx :health] inc)
+    (swap! *health* - x)))
+
+;; Slime
+(defrecord slime [health sliminess])
+(defn make-slime []
+  (slime. (randval 10) (randval 5)))
+(derive slime ::foe)
+(swap! *foe-builders* conj make-slime)
+
+(defmethod foe-show slime [f]
+  (str "A slime with a sliminess of " (:sliminess f)))
+
+(defmethod foe-attack slime [idx f]
+  (let [x (randval (:sliminess f))]
+    (println "A slime wraps your legs and decreases your agility by" x)
+    (swap! *agility* - x)
+    (when (zero? (rand-int 2))
+      (println "It also squirts your face, taking away 1 health point!")
+      (swap! *health* dec))))
+
+;; Brigand
+(defrecord brigand [health])
+(defn make-brigand []
+  (brigand. (randval 10)))
+(derive brigand ::foe)
+(swap! *foe-builders* conj make-brigand)
+
+(defmethod foe-attack brigand [idx f]
+  (let [x (max @*health* @*agility* @*strength*)]
+    (cond
+     (= x @*health*) (do
+		       (println "A brigand hits you with his slingshot.")
+		       (println "Takes off 2 health points!")
+		       (swap! *health* - 2))
+     (= x @*agility*) (do
+			(println "A brigand whips your leg.")
+			(println "Takes off 2 agility points!")
+			(swap! *agility* - 2))
+     :else (do
+	     (println "A brigand whips your arm.")
+	     (println "Takes off 2 strength points!")
+	     (swap! *strength* - 2)))))
+
+(defn init-foes []
+  (let [new-foes (repeatedly *foes-num* #((rand-nth @*foe-builders*)))]
+    (reset! *foes* (vec new-foes))))
+
+(defn show-foes []
+  (println "Your foes:")
+  (doseq [[x f] (indexed @*foes*)]
+    (println (inc x)
+	     (if (foe-dead? f)
+	       "**dead**"
+	       (str "(Health = " (:health f) ") " (foe-show f))))))
+
+(defn random-foe []
+  (let [[x f] (rand-nth (indexed @*foes*))]
+    (if (foe-dead? f)
+      (recur)
+      [x f])))
+
+(defn pick-foe []
+  (println "Foe #:")
+  (let [x (read)]
+    (if-not (and (integer? x) (>= x 1) (<= x *foes-num*))
+      (do (println "That is not a valid foe number.")
+	  (recur))
+      (let [f (@*foes* (dec x))]
+	(if (foe-dead? f)
+	  (do (println "That foe is already dead.")
+	      (recur))
+	  [(dec x) f])))))
+
+(defn init-player []
+  (reset! *health* 30)
+  (reset! *agility* 30)
+  (reset! *strength* 30))
+
+(defn player-dead? []
+  (<= @*health* 0))
+
+(defn show-player []
+  (println "Health:" @*health*
+	   "Agility:" @*agility*
+	   "Strength:" @*strength*))
+
+(defn player-attack []
+  (println "Attack: [s]tab [d]ouble swing [r]oundhouse:")
+  (case (read)
+	's (let [x (+ 2 (randval (quot @*strength* 2)))]
+	     (println "Stab has strength of" x)
+	     (foe-hit (pick-foe) x))
+	'd (let [x (randval (quot @*strength* 6))]
+	     (println "Double swing has strength of" x)
+	     (foe-hit (pick-foe) x)
+	     (if-not (foes-dead?)
+	       (foe-hit (pick-foe) x)))
+	'r (dotimes [_ (inc (randval (quot @*strength* 3)))]
+	     (if-not (foes-dead?)
+	       (foe-hit (random-foe) 1)))
+	(recur)))
+
+(defn game-loop []
+  (when-not (or (player-dead?) (foes-dead?))
+    (show-player)
+    (dotimes [_ (inc (quot @*agility* 15))]
+      (when-not (foes-dead?)
+	(show-foes)
+	(player-attack)))
+    (doseq [[idx f] (indexed @*foes*)]
+      (if-not (foe-dead? f)
+	(foe-attack idx f)))
+    (recur)))
+
+(defn battle []
+  (init-foes)
+  (init-player)
+  (game-loop)
+  (cond
+   (player-dead?) (println "You have been killed. Game over.")
+   (foes-dead?) (println "Congratulations! You vanquished all foes.")))
+
+(battle)
---
+++ Land of Lisp/ch9/ch9.clj
@@ -0,0 +1,164 @@
+;; Clojure vectors
+[nil nil nil]
+(def x (atom [nil nil nil]))
+(nth @x 1)
+(swap! x assoc 1 'foo)
+@x
+(nth @x 1)
+
+;; Clojure lists can't be 'modified' at will
+(def foo (atom '[a b c]))
+(second @foo)
+(swap! foo assoc 1 'z)
+@foo
+(reset! foo [nil nil nil nil])
+(swap! foo assoc 2 '[x y z])
+@foo
+(swap! foo assoc-in [2 0] {})
+(swap! foo assoc-in [2 0 'zoink] 5)
+@foo
+
+(nth '(foo bar baz) 1)
+
+{}
+(def x (atom {}))
+(@x 'yup)
+(def x (atom {}))
+(swap! x assoc 'yup '25)
+(@x 'yup)
+(def *drink-order* (atom {}))
+(swap! *drink-order* assoc 'bill 'double-espresso)
+(swap! *drink-order* assoc 'lisa 'small-drop-coffee)
+(swap! *drink-order* assoc 'john 'medium-latte)
+(@*drink-order* 'lisa)
+
+(use '[clojure.contrib.math :only (round)])
+(round 2.4)
+(defn foo []
+  [3 7])
+(foo)
+(+ (first (foo)) 5)
+(let [[a b] (foo)]
+  (* a b))
+
+(def *node-num* 1000)
+(def *edge-num* 1000)
+(defn random-node []
+  (inc (rand-int *node-num*)))
+(defn edge-pair [a b]
+  (if (not= a b)
+    [[a b] [b a]]))
+(defn make-edge-list []
+  (apply concat (repeatedly *edge-num*
+			    #(edge-pair (random-node) (random-node)))))
+(defn direct-edges [node edge-list]
+  (filter #(= node (first %)) edge-list))
+(defn get-connected [node edge-list]
+  (loop [[n & ns] [node]
+	 connected #{}]
+    (if-not n
+      connected
+      (let [neighbors (distinct (map second (direct-edges n edge-list)))]
+	(recur (concat ns (remove connected neighbors))
+	       (conj connected n))))))
+(time (dotimes [_ 100] (get-connected 1 (make-edge-list))))
+(defn edges-to-map [edge-list]
+  (into {} (for [[n1 _] edge-list]
+	     [n1 (distinct (map second (direct-edges n1 edge-list)))])))
+(defn get-connected-hash [node edge-map]
+  (loop [[n & ns] [node]
+	 connected #{}]
+    (if-not n
+      connected
+      (let [neighbors (edge-map node)]
+	(recur (concat ns (remove connected neighbors))
+	       (conj connected n))))))
+(time (dotimes [_ 100]
+	(get-connected-hash 1 (edges-to-map (make-edge-list)))))
+
+(defrecord person [name age waist-size favorite-color])
+(def *bob* (atom (person. "Bob" 35 32 "blue")))
+@*bob*
+(:age @*bob*)
+(swap! *bob* assoc :age 36)
+;; Clojure records don't have print/read symmetry
+(def *that-guy* {:name "Bob", :age 36, :waist-size 32, :favorite-color "blue"})
+(:age *that-guy*)
+
+(defn make-person [name age waist-size favorite-color]
+  (list name age waist-size favorite-color))
+(defn person-age [person]
+  (fnext person))
+(def *bob* (make-person "bob" 35 32 "blue"))
+*bob*
+(person-age *bob*)
+
+(count '(a b c))
+(count "blub")
+(count [nil nil nil nil nil])
+
+(use '[clojure.contrib.seq :only (find-first positions)])
+(find-first number? '(a b 5 d))
+(count (filter #{\s} "mississippi"))
+(first (positions #{\4} "2kewl4skewl"))
+(some number? '(a b 5 d))
+(every? number? '(a b 5 d))
+
+(reduce + '(3 4 6 5 2))
+(reduce (fn [best item]
+	  (if (and (even? item) (> item best))
+	    item
+	    best))
+	0
+	'(7 4 6 5 2))
+(reduce (fn [best item]
+	  (if (and (even? item) (> item best))
+	    item
+	    best))
+	'(7 4 6 5 2))
+(defn sum [lst]
+  (reduce + lst))
+(sum '(1 2 3))
+(sum [1 2 3 4 5])
+(sum "blablabla")
+(map #(if (= % \s)
+	\S
+	%)
+     "this is a string")
+
+(subs "america" 2 6)
+(sort < '(5 8 2 4 9 3 6))
+
+(number? 5)
+(defn add [a b]
+  (cond
+   (and (number? a) (number? b)) (+ a b)
+   (and (list? a) (list? b)) (concat a b)))
+(add 3 4)
+(add '(a b) '(c d))
+(defmulti add (fn [a b] [(type a) (type b)]))
+(defmethod add [java.lang.Integer java.lang.Integer] [a b]
+  (+ a b))
+(defmethod add [clojure.lang.PersistentList
+		clojure.lang.PersistentList] [a b]
+		(concat a b))
+(add 3 4)
+(add '(a b) '(c d))
+
+(dotimes [i 3]
+  (println i ". Hatchoo!"))
+
+(dotimes [_ 10]
+  (println (rand-int 5)))
+
+(use 'battle)
+(defrecord foe [health])
+(defn make-foe []
+  (foe. (randval 10)))
+(make-foe)
+(make-foe)
+(make-foe)
+(type 'foo)
+(type 5)
+(type "foo")
+(type (make-foe))
