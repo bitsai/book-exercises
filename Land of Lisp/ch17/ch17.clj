ab17731d5dbf38046abd263bb2f37332c74fb11d
Benny Tsai <benny.tsai@gmail.com>
2011-07-13 21:31:41 -0600

    revamp chapter 17 Clojure code

--- Land of Lisp/ch17/ch17.clj
+++ Land of Lisp/ch17/ch17.clj
@@ -1,23 +1,6 @@
-(require '[clojure.string :as str])
-(defn print-tag [name attributes closing?]
-  (print \<)
-  (when closing?
-    (print \/))
-  (print (str/lower-case name))
-  (doseq [[attribute value] attributes]
-    (print (format " %s=\"%s\"" (str/lower-case attribute) value)))
-  (print \>))
+(use 'svg)
 (print-tag 'mytag '[[color blue] [height 9]] false)
 
-(defn pairs [coll]
-  (partition 2 coll))
-(defmacro tag [name attributes & body]
-  `(do (print-tag '~name
-                  (vector ~@(for [[attribute value] (pairs attributes)]
-                              `(vector '~attribute ~value)))
-                  false)
-       ~@body
-       (print-tag '~name nil true)))
 (tag mytag [color 'blue height (+ 4 5)])
 (macroexpand '(tag mytag [color 'blue height (+ 4 5)]))
 (tag mytag [color 'blue size 'big]
@@ -28,25 +11,16 @@
      (tag body []
           (print "Hello World!")))
 (defmacro html [& body]
-  `(tag "html" []
+  `(tag ~'html []
         ~@body))
 (defmacro body [& body]
-  `(tag "body" []
+  `(tag ~'body []
         ~@body))
 (html
  (body
   (print "Hello World!")))
 
-(defmacro svg [& body]
-  `(tag "svg" ["xmlns" "http://www.w3.org/2000/svg"
-               "xmlns:xlink" "http://www.w3.org/1999/xlink"]
-        ~@body))
-(defn brightness [col amt]
-  (map #(min 255 (max 0 (+ % amt))) col))
 (brightness [255 0 0] -100)
-(defn svg-style [color]
-  (apply format "fill:rgb(%s,%s,%s);stroke:rgb(%s,%s,%s)"
-         (into color (brightness color -100))))
 (defn circle [center radius color]
   (tag circle [cx (first center)
                cy (second center)
@@ -55,9 +29,6 @@
 (svg (circle [50 50] 50 [255 0 0])
      (circle [100 100] 50 [0 0 255]))
 
-(defn polygon [points color]
-  (tag polygon [points (str/join (for [[x y] points] (str x "," y " ")))
-                style (svg-style color)]))
 (defn random-walk [value length]
   (when-not (zero? length)
     (cons value
