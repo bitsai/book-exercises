cdfb2320872b35cc544f7d89dd002b5110e5e0dc
Benny Tsai <benny.tsai@gmail.com>
2011-07-16 12:11:56 -0600

    revamp Land of Lisp chapter 6.5 Clojure code

--- Land of Lisp/ch6.5/ch6.5.clj
+++ Land of Lisp/ch6.5/ch6.5.clj
@@ -1,10 +1,7 @@
 (defn half [n]
   (/ n 2))
-
 half
-
 (fn [n] (/ n 2))
 #(/ % 2) ;; Clojure shorthand for lambdas
-
-(map (fn [n] (/ n 2)) '(2 4 6))
-(map #(/ % 2) '(2 4 6))
+(map (fn [n] (/ n 2)) [2 4 6])
+(map #(/ % 2) [2 4 6])
