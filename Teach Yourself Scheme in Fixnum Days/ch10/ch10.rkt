8412b66252fd09523be43caaab796d8a19b1f289
Benny Tsai <benny.tsai@gmail.com>
2011-08-27 22:05:39 -0600

    put table fns in separate module, add tests

--- Teach Yourself Scheme in Fixnum Days/ch10/ch10.rkt
+++ Teach Yourself Scheme in Fixnum Days/ch10/ch10.rkt
@@ -1,28 +1,19 @@
 #lang racket
 
-(require "struct.rkt")
+(require "table.rkt")
 
-(defstruct table (equ eqv?) (alist '()))
-(define (table-get tbl k . d)
-  (let ((c (lassoc k (table.alist tbl) (table.equ tbl))))
-    (cond (c (mcdr c)) ;;Use mcdr
-          ((pair? d) (car d)))))
-(define (lassoc k al equ?)
-  (let loop ((al al))
-    (if (null? al)
-        #f
-        (let ((c (car al)))
-          (if (equ? (mcar c) k) ;;Use mcar
-              c
-              (loop (cdr al)))))))
-(define (table-put! tbl k v)
-  (let* ((al (table.alist tbl))
-         (c (lassoc k al (table.equ tbl))))
-    (if c
-        (set-mcdr! c v) ;;Use set-mcdr!
-        (set!table.alist tbl (cons (mcons k v) al))))) ;;Use mcons
-(define (table-foreach tbl p)
-  (for-each
-   (lambda (c)
-     (p (mcar c) (mcdr c))) ;;Use mcar & mcdr
-   (table.alist tbl)))
+(define t (make-table))
+(table? t)
+(table? #())
+(table-get t 0)
+(table-put! t 0 0)
+(table-get t 0)
+(table-put! t 0 "a")
+(table-put! t 1 "b")
+(table-put! t 2 "c")
+(table-for-each t
+                (lambda (k v)
+                  (display k)
+                  (display " ")
+                  (display v)
+                  (newline)))
