22189c63b7ad7189b5843b5efb0c0938ea8f43c8
Benny Tsai <benny.tsai@gmail.com>
2011-08-28 00:25:35 -0600

    chapter 12 of Teach Yourself Scheme in Fixnum Days

---
+++ Teach Yourself Scheme in Fixnum Days/ch12/ch12.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+(require "object.rkt")
+
+(define trivial-bike-class (make-standard-class
+                            'supers '()
+                            'slots '(frame size parts)
+                            'method-names '()
+                            'method-vector #()))
+(define my-bike (make-instance trivial-bike-class
+                               'frame 'cromoly
+                               'size '18.5
+                               'parts 'alivio))
+my-bike
+(class-of my-bike)
+(slot-value my-bike 'frame)
+(set!slot-value my-bike 'frame 'unobtanium)
+(slot-value my-bike 'frame)
+(define bike-class (create-class
+                    ()
+                    (frame size parts chain tires)
+                    (check-fit (lambda (me inseam)
+                                 (let* ((bike-size (slot-value me 'size))
+                                        (ideal-size (* inseam 3/5))
+                                        (diff (- bike-size ideal-size)))
+                                   (cond ((<= -1 diff 1) 'perfect-fit)
+                                         ((<= -2 diff 2) 'fits-well)
+                                         ((< diff -2) 'too-small)
+                                         ((> diff 2) 'too-big)))))))
+(define my-bike (make-instance bike-class
+                               'frame 'titanium
+                               'size 21
+                               'parts 'ultegra
+                               'chain 'sachs
+                               'tires 'continental))
+(send 'check-fit my-bike 34)
+(class-of my-bike)
+(slot-value my-bike 'suspension)
+(set!slot-value my-bike 'suspension 'awesome)
+(slot-value my-bike 'suspension)
+(define mtn-bike-class (create-class
+                        (bike-class)
+                        (suspension)
+                        (check-fit (lambda (me inseam)
+                                     (let* ((bike-size (slot-value me 'size))
+                                            (ideal-size (- (* inseam 3/5) 2))
+                                            (diff (- bike-size ideal-size)))
+                                       (cond ((<= -2 diff 2) 'perfect-fit)
+                                             ((<= -4 diff 4) 'fits-well)
+                                             ((< diff -4) 'too-small)
+                                             ((> diff 4) 'too-big)))))))
+(define my-bike (make-instance mtn-bike-class
+                               'frame 'titanium
+                               'size 21
+                               'parts 'ultegra
+                               'chain 'sachs
+                               'tires 'continental
+                               'suspension 'none))
+(send 'check-fit my-bike 34)
+(class-of my-bike)
+(slot-value my-bike 'suspension)
+(set!slot-value my-bike 'suspension 'awesome)
+(slot-value my-bike 'suspension)
---
+++ Teach Yourself Scheme in Fixnum Days/ch12/object.rkt
@@ -0,0 +1,132 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(require (lib "defmacro.ss"))
+
+(define (list-position x lst)
+  (let loop ((i 0)
+             (lst lst))
+    (cond ((null? lst) #f)
+          ((eqv? x (car lst)) i)
+          (else (loop (+ i 1)
+                      (cdr lst))))))
+
+(define (delete-duplicates lst)
+  (if (null? lst)
+      lst
+      (let ((x (car lst))
+            (xs (cdr lst)))
+        (if (memv x xs)
+            (delete-duplicates xs)
+            (cons x (delete-duplicates xs))))))
+
+(define (append-map f lst)
+  (let loop ((lst lst))
+    (if (null? lst)
+        '()
+        (append (f (car lst))
+                (loop (cdr lst))))))
+
+(define-macro (create-class direct-supers slots . methods)
+  `(create-class-proc
+    (list ,@(map (lambda (su) `,su) direct-supers))
+    (list ,@(map (lambda (slot) `',slot) slots))
+    (list ,@(map (lambda (method) `',(car method)) methods))
+    (vector ,@(map (lambda (method) `,(cadr method)) methods))))
+
+(define (make-instance class . slot-value-pairs)
+  (let* ((slots (class-slots class))
+         (n (length slots))
+         (instance (make-vector (+ n 1))))
+    (vector-set! instance 0 class)
+    (let loop ((slot-value-pairs slot-value-pairs))
+      (if (null? slot-value-pairs)
+          instance
+          (let ((k (list-position (car slot-value-pairs) slots)))
+            (vector-set! instance (+ k 1) (cadr slot-value-pairs))
+            (loop (cddr slot-value-pairs)))))))
+
+(define standard-class (vector 'place-holder
+                               (list 'slots
+                                     'supers
+                                     'method-names
+                                     'method-vector)
+                               '()
+                               '(make-instance)
+                               (vector make-instance)))
+
+(vector-set! standard-class 0 standard-class)
+
+(define (make-standard-class . args)
+  (apply send 'make-instance standard-class args))
+
+(define (standard-class? x)
+  (and (vector? x)
+       (>= (vector-length x) 1)
+       (eqv? (vector-ref x 0) standard-class)))
+
+(define (class-slots c)
+  (vector-ref c 1))
+
+(define (class-supers c)
+  (vector-ref c 2))
+
+(define (class-method-names c)
+  (vector-ref c 3))
+
+(define (class-method-vector c)
+  (vector-ref c 4))
+
+(define (class-of x)
+  (if (vector? x)
+      (let ((n (vector-length x)))
+        (if (>= n 1)
+            (let ((class (vector-ref x 0)))
+              (if (standard-class? class)
+                  class
+                  #t))
+            #t))
+      #t))
+
+(define (slot-value instance slot)
+  (let* ((class (class-of instance))
+         (k (list-position slot (class-slots class))))
+    (if k
+        (vector-ref instance (+ k 1))
+        #f)))
+
+(define (set!slot-value instance slot value)
+  (let* ((class (class-of instance))
+         (k (list-position slot (class-slots class))))
+    (if k
+        (vector-set! instance (+ k 1) value)
+        #f)))
+
+(define (create-class-proc direct-supers slots method-names method-vector)
+  (let ((supers (delete-duplicates (append direct-supers
+                                           (append-map (lambda (c)
+                                                         (class-supers c))
+                                                       direct-supers)))))
+    (make-standard-class
+     'supers supers
+     'slots (delete-duplicates (append slots
+                                       (append-map (lambda (c)
+                                                     (class-slots c))
+                                                   supers)))
+     'method-names method-names
+     'method-vector method-vector)))
+
+(define (send method-name instance . args)
+  (let ((proc (let ((class (class-of instance)))
+                (if (eqv? #t class)
+                    (error 'send)
+                    (let loop ((class class)
+                               (supers (class-supers class)))
+                      (let* ((method-names (class-method-names class))
+                             (k (list-position method-name method-names)))
+                        (cond (k (vector-ref (class-method-vector class) k))
+                              ((null? supers) (error 'send))
+                              (else (loop (car supers)
+                                          (cdr supers))))))))))
+    (apply proc instance args)))
