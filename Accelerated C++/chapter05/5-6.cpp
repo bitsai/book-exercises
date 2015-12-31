ad4f45ca9364c7cad75e2ec3a3d0605d6de61d40
Benny Tsai <benny.tsai@gmail.com>
2015-12-30 17:48:48 -0800

    fix bug in Accelerated C++ exercise 5-6 (thank you @a2mk!)

--- Accelerated C++/chapter05/5-6.cpp
+++ Accelerated C++/chapter05/5-6.cpp
@@ -26,21 +26,24 @@ vector<Student_info> extract_fails(vector<Student_info>& students) {
   vector<Student_info> fail;
 #ifdef _MSC_VER
   std::vector<Student_info>::size_type i = 0;
+  std::vector<Student_info>::size_type rawsize = students.size();
 #else
   vector<Student_info>::size_type i = 0;
+  vector<Student_info>::size_type rawsize = students.size();
 #endif
 
   // invariant: elements `[0,' `i)' of `students' represent passing grades
   while (i != students.size()) {
     if (fgrade(students[i])) {
       fail.push_back(students[i]);
+    } else {
       students.insert(students.begin(), students[i]);
       ++i;
     }
     ++i;
   }
     
-  students.resize(students.size() - fail.size());
+  students.resize(rawsize - fail.size());
 
   return fail;
 }
@@ -61,6 +64,9 @@ int main() {
   clock_t elapsed = clock() - start;
 
   cout << "Elapsed: " << elapsed << endl;
+
+  for (int i = 0; i < vs.size(); ++i)
+    cout << vs[i].name << " " << grade(vs[i]) << endl;
 
   //  for (int i = 0; i < fails.size(); ++i)
   //    cout << fails[i].name << " " << grade(fails[i]) << endl;
