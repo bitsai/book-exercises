4aed32af2d3ec7b0a3f6da4550a6e898445c1745
Benny Tsai <benny.tsai@gmail.com>
2016-01-30 18:09:19 -0800

    go back and annotate Clojure day 1

--- Seven Languages in Seven Weeks/Clojure/day_1.clj
+++ Seven Languages in Seven Weeks/Clojure/day_1.clj
@@ -3,8 +3,25 @@
 (defn big [st n]
   (> (count st) n))
 
+;; day-1> (big "hello world" 9)
+;; true
+;; day-1> (big "hello world" 10)
+;; true
+;; day-1> (big "hello world" 11)
+;; false
+
 (defn collection-type [col]
   (cond
     (list? col)   :list
     (map? col)    :map
-    (vector? col) :vector))
+    (vector? col) :vector
+    :else         :unknown))
+
+;; day-1> (collection-type '())
+;; :list
+;; day-1> (collection-type {})
+;; :map
+;; day-1> (collection-type [])
+;; :vector
+;; day-1> (collection-type #{})
+;; :unknown
