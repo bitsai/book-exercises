21e0ff88f445844062a48aaaeb98eba0b86c9031
Benny Tsai <benny.tsai@gmail.com>
2016-01-30 18:46:59 -0800

    annotate Erlang day 2

--- Seven Languages in Seven Weeks/Erlang/day_2.erl
+++ Seven Languages in Seven Weeks/Erlang/day_2.erl
@@ -5,7 +5,15 @@ find([], _) -> not_found;
 find([{Keyword, Value}|_], Keyword) -> Value;
 find([_|Tail], Keyword) -> find(Tail, Keyword).
 
+%% 4> day_2:find([{erlang, "a functional language"}, {ruby, "an OO language"}], erlang).
+%% "a functional language"
+%% 5> day_2:find([{erlang, "a functional language"}, {ruby, "an OO language"}], prolog).
+%% not_found
+
 total_prices(ShoppingList) -> [{Item, Quantity * Price} || {Item, Quantity, Price} <- ShoppingList].
+
+%% 6> day_2:total_prices([{hot_dog, 3, 1.50}, {smoothie, 1, 1.65}, {pizza, 1, 2.0}]).
+%% [{hot_dog,4.5},{smoothie,1.65},{pizza,2.0}]
 
 %% 3 possible horizontal victories
 tic_tac_toe([Z,Z,Z,_,_,_,_,_,_]) when Z == x orelse Z == o -> Z;
@@ -26,3 +34,10 @@ tic_tac_toe(Moves) ->
         %% no winner yet
         false -> no_winner
     end.
+
+%% 8> day_2:tic_tac_toe([nil,nil,nil,nil,nil,nil,nil,nil,nil]).
+%% no_winner
+%% 9> day_2:tic_tac_toe([o,x,x,o,x,nil,o,nil,nil]).
+%% o
+%% 10> day_2:tic_tac_toe([o,x,o,o,x,x,x,o,x]).
+%% cat
