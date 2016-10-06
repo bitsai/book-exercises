a93292b746f550388432444ebfe2b3ff9b01eb1e
Benny Tsai <benny.tsai@gmail.com>
2016-10-06 11:16:36 -0700

    fix quartiles calculation for Accelerated C++ 3-2 (fixes #2)

--- Accelerated C++/chapter03/3-2.cpp
+++ Accelerated C++/chapter03/3-2.cpp
@@ -4,12 +4,26 @@
 
 using namespace std;
 
+#ifdef _MSC_VER
+  typedef std::vector<int>::size_type vec_sz;
+#else
+  typedef vector<int>::size_type vec_sz;
+#endif
+
+double median(const vector<int>& integers, vec_sz begin, vec_sz end) {
+  vec_sz size = begin + end;
+  vec_sz mid = size / 2;
+  return size % 2 == 0 ?
+    (integers[mid] + integers[mid - 1]) / 2.0 :
+    integers[mid];
+}
+
 int main() {
   vector<int> integers;
+  int x;
 
   cout << "Integers: ";
-  int x;
-	
+
   while (cin >> x)
     integers.push_back(x);
 
@@ -17,25 +31,32 @@ int main() {
     cout << endl << "No integers!" << endl;
     return 1;
   }
-	
-  sort(integers.rbegin(), integers.rend());
-  typedef vector<int>::size_type vec_sz;
 
-  cout << "1st quartile" << endl;
-  for (vec_sz i = 0; i < integers.size() / 4; ++i)
-    cout << integers[i] << endl;
+  sort(integers.begin(), integers.end());
 
-  cout << "2nd quartile" << endl;
-  for (vec_sz i = integers.size() / 4; i < integers.size() / 2; ++i)
-    cout << integers[i] << endl;
+  vec_sz mid = integers.size() / 2;
+  vec_sz lower_half_end;
+  vec_sz upper_half_begin;
 
-  cout << "3rd quartile" << endl;
-  for (vec_sz i = integers.size() / 2; i < integers.size() * 3 / 4; ++i)
-    cout << integers[i] << endl;
+  // compute quartiles using Method 1 from: https://en.wikipedia.org/wiki/Quartile
+  if (integers.size() % 2 != 0) {
+    // odd number of data points, exclude median from either half
+    lower_half_end = mid;
+    upper_half_begin = mid + 1;
+  } else {
+    // even number of data points, split data set exactly in half
+    lower_half_end = mid;
+    upper_half_begin = mid;
+  }
 
-  cout << "4th quartile" << endl;
-  for (vec_sz i = integers.size() * 3 / 4; i < integers.size(); ++i)
-    cout << integers[i] << endl;
+  cout << "Q1" << endl;
+  cout << median(integers, 0, lower_half_end) << endl;
+
+  cout << "Q2" << endl;
+  cout << median(integers, 0, integers.size()) << endl;
+
+  cout << "Q3" << endl;
+  cout << median(integers, upper_half_begin, integers.size()) << endl;
 
   return 0;
 }
