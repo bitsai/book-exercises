4ac3fd7d9780375a4c05ae0be7937b0fb6e93841
Benny Tsai <benny.tsai@gmail.com>
2017-02-16 10:51:44 -0800

    update test case

--- Accelerated C++/chapter15/15-5.cpp
+++ Accelerated C++/chapter15/15-5.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main() {
   Vec<Str> xs;
-  xs.push_back("1111");
-  xs.push_back("2222");
-  xs.push_back("3333");
-  xs.push_back("4444");
+  xs.push_back("AAAA");
+  xs.push_back("AA");
+  xs.push_back("AA");
+  xs.push_back("AAAA");
 
   Vec<Str> ys;
-  ys.push_back("AA");
+  ys.push_back("BB");
   ys.push_back("BB");
 
   Picture p(xs);
