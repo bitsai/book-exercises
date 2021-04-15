06a19bbe3c1baa149e75f6ab509b15eb8a645b8d
BharatThapa99 <57470581+BharatThapa99@users.noreply.github.com>
2021-04-15 06:40:40 -0400

    Update 2-5.cpp
    Flexible approach to problem 2-5 using loops

--- Accelerated C++/chapter02/2-5.cpp
+++ Accelerated C++/chapter02/2-5.cpp
@@ -1,16 +1,95 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-  cout << "**" << endl;
-  cout << "**" << endl << endl;
+int main()
+{
+    /*
+    for square we have rows = cols but for
+    rectangls rows and cols are not equal
+   */
 
-  cout << "***" << endl;
-  cout << "***" << endl << endl;
+   cout << "Which shape you want to print? 1. square 2. rectangle 3. Triangle: " << endl;
+   int choice;
+   cin >> choice;
+   int rows;
+   int cols; 
+   string::size_type c = 0;
+ 
+   if (choice == 1 || choice == 2)
+   {
+        cout << "Enter Length: ";
+        cin >> cols;
+        rows = cols;
+        cout << endl;
+       
+       const string spaces(cols - 2, ' ');
+       for (int r = 0; r < rows; r++)
+       {
+           for ( c = 0; c < cols; c++)
+           {
+               
+               if (r == 0 || r == rows -1 || c == 0 || c == cols -1)
+               {
+                   
+                //    cout << "r = " << r << " c = " << c << endl;
+                   cout << "* ";
+               }
+               else
+               {
+                   cout << spaces << spaces;
+                   c += (spaces.size()-1);// or you can print spaces one character at a time by replacing both line with only cout << "  ";
+               }
+               
+               
+               
+           }
+           
+           cout << endl;
+       }
+       
 
-  cout << "*" << endl;
-  cout << "**" << endl;
+   }
+   else
+   {
+       /*                             *
+                         *           * *
+                *       * *         * * *
+         *     * *     * * *       * * * *
+    *   * *   * * *   * * * *     * * * * *
+    */    
+        cout << "Enter base: ";
+        cin >> rows;
+        
+        int r = 0;
+        int c = rows;
+       for (int r = 0; r < rows; r++)
+       {
+           
+           for ( c = rows; c > r; c--)
+           {
+               /* code */
+               cout << " ";
+             
+               
+           }
+            for (int star = 0; star <= c; star++)
+            {
+                /* code */
+                cout << "* ";
 
-  return 0;
+            }
+           cout << endl;
+           
+       }
+       
+       
+       
+   }
+   
+
+   
+
+    return 0;
 }
